a5e0eba984431e1ff39c08418bc92a765db48ed8
aditya24 <adityadawar1999@gmail.com>
2024-06-19 15:01:08 +0530

    shift

--- src/components/resume/Experiences.js
+++ src/components/resume/Experiences.js
@@ -14,13 +14,6 @@ export default function Experiences() {
         <div>
           <div className="w-full border-l-[6px] border-black border-opacity-30 flex flex-col gap-10" data-aos="zoom-in">
             <ResumeCard
-              title="Frontend Dev Intern"
-              subTitle="Robotronix"
-              studyYear="(May 2023 - July 2023)"
-              result="Remote"
-              desc="Built a responsive E-commerce site with Tailwind CSS, and ReactJS, showcasing 1000+ products. Integrated a Payment Gateway and Google Maps, managing state with React hooks and APIs, processing 500+ transactions daily"
-            />
-            <ResumeCard
               title="Intern"
               subTitle="Safron Umbrella"
               studyYear="(Dec 2023 - Jan 2024)"
@@ -33,6 +26,13 @@ export default function Experiences() {
               studyYear="(June 2023 - June 2023)"
               result="IIT Roorkee"
               desc="Led a 5-member team under Prof. Sumit Kumar Yadav at IIT Roorkee. Developed an image compression project using Gaussian Mixture Model, achieving 40% compression and 97.9% accuracy with resizing techniques and iterative component adjustments"
+            />
+             <ResumeCard
+              title="Frontend Dev Intern"
+              subTitle="Robotronix"
+              studyYear="(May 2023 - July 2023)"
+              result="Remote"
+              desc="Built a responsive E-commerce site with Tailwind CSS, and ReactJS, showcasing 1000+ products. Integrated a Payment Gateway and Google Maps, managing state with React hooks and APIs, processing 500+ transactions daily"
             />
           </div>
         </div>
